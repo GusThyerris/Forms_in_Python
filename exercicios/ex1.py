54d8e90cb31fc1ebe8efd7d302bd734b874efd73
gusThyerris <gustavothyerrisdonascimentooli@gmail.com>
2024-03-26 22:24:14 -0300

    update de comentários explicativos

--- exercicios/ex1.py
+++ exercicios/ex1.py
@@ -12,13 +12,17 @@
 # obs.: sem usar a idéia da função.
 # 
 
+# Criação da variável raio e atribuíção do valor como um número real digitado pelo usuário
 raio = float(input("Digite o raio da esfera: "))
 
+# Criação da variável comprimento e atribuindo a formula, depois imprimindo o valor
 comprimento = 2 * raio
 print("Comprimento da esfeta: ", comprimento)
 
+# Criação da variável area e atribuindo a formula, depois imprimindo o valor
 area = raio ** 2
 print("Área da esfera: ", area)
 
+# Criação da variável volume e atribuindo a formula(considerando pi = 3.14159265359), depois imprimindo o valor
 volume = (4/3) * 3.14159265359 * raio ** 3
 print("Volume da esfera: ", volume)
--- exercicios/ex2.py
+++ exercicios/ex2.py
@@ -1,17 +1,22 @@
 # 2)Faça o exercício 1, só que agora crie três funções que retornem o valor do comprimento,
 # área e do volume. Depois, faça a média aritimética dos volumes de 3 esferas com raio 30, 40 e 50mm.
 
+# Criação da função que executará o calculo do comprimento
 def calcular_comprimento(raio):
     return 2 * raio
 
+# Criação da função que executará o calculo da área
 def calcular_area(raio):
     return raio ** 2
 
+# Criação da função que executará o calculo do volume(considerando pi = 3.141592)
 def calcular_volume(raio):
     return (4/3) * 3.141592 * raio ** 3
 
+# Atribuindo valores ao vetor raio, depois calculando o volume com cada valor do vetor
 raios = [30, 40, 50]
 volumes = [calcular_volume(raio) for raio in raios]
 
+# Criando a variável que recebe o calculo da média aritmética dos volumes, depois imprimindo o valor para o usuário
 media_volumes = sum(volumes) / len(volumes)
 print("Média do volume das esferas: ", media_volumes)
